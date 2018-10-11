137890f3131a46ca343ab5b96e76199b5a806cdf
Alex Dubois <alex@bowabos.com>
2018-10-11 11:11:04 +0100

    postgres integration for books

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
-#[macro_use()]
+#[macro_use]
 extern crate diesel;
-#[macro_use()]
-extern crate diesel_codegen;
 extern crate dotenv;
 
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-mod models;
-mod schema;
+pub mod models;
+pub mod schema;
 
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let conn = PgConnection::establish(&database_url)
+        .expect(&format!("Error connecting to {}", database_url));
+
+    let book = models::NewBook {
+        title: String::from("Gravity's Rainbow"),
+        author: String::from("Thomas Pynchon"),
+        published: true,
+    };
+
+    if models::Book::insert(book, &conn) {
+        println!("success");
+    } else {
+        println!("failed");
+    }
 }
